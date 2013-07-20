96e8ed4336e95674c2c91bd82b7f38fb2fb85c0b
Rene Martinez <martinez.rene1@gmail.com>
2013-07-20 20:12:11 +0000

    Updated it

--- web.js
+++ web.js
@@ -1,17 +1,11 @@
 var express = require('express');
-var fs = require('fs');
+var app = express();
+app.use(express.logger());
 
-var data = fs.readFileSync("index.html","utf-8");
-
-var app = express.createServer(express,logger());
 
 app.get('/', function(request, response) {
 
-   response.send(data);
-
-
-
-    
+   response.send('Hello World!');
 
 });
 
