eff94821ad5df0b637d12938d387f928485fa523
Rene Martinez <martinez.rene1@gmail.com>
2013-07-20 22:07:33 +0000

    got error with buffer web.js

--- web.js
+++ web.js
@@ -57,19 +57,23 @@
 
 
 
-var express = require('express');
+var fs = require("fs");
 
- var app = express.createServer(express.logger());
+var express = require("express");
 
- app.get('/', function(request, response) {
-    var fs = require('fs');
-     var mybuf = fs.readFileSync("index.html");
+var buffer = new Buffer(128);
 
-    var buffer = new Buffer();
-    response.send(buffer.toString('utc-8', mybuf.toString()));
+// buffer = fs.readFileSync("./index.html");
+
+   var app = express.createServer(express.logger());
+// var app = express();
+
+// console.log("Buffer is: " + buffer.toString('utf8'));
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync("./index.html").toString()) ;
 });
 
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
   console.log("Listening on " + port);
 });
