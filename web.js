a7fbfae1722ac72efa1853844a817d1da3cba9e3
Rene Martinez <martinez.rene1@gmail.com>
2013-07-21 01:01:33 +0000

    final code for web.js

--- web.js
+++ web.js
@@ -1,57 +1,8 @@
 /** code for hemeowrk #3
-
 * by : Rene M
 */
 
 
-
-//**ar express = require('express');
-//var app = express();
-
-
-//app.use(express.logger());
-
-
-//app.get('/', function(request, response) {
-
-
-  //   response.send('Hello World2!');
-
-//});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-  //console.log("Listening on " + port);
-//});
-
-
-
-
-//*******************************************
-//var express = require('express');
-//var fs = require('fs');
-
-//var data = fs.readFileSync("index.html","utf-8");
-
-
-//var app = express.createServer(express.logger());
-
-
-
-
-//app.get('/', function(request, response) {
-
-  //response.send(data);
-
-
-//});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-  //console.log("Listening on " + port);
-//});
-
-//************************************************
 
 
 
@@ -59,10 +10,10 @@ var express = require('express');
 var fs = require('fs');
 var buff = new Buffer(fs.readFileSync("index.html"));
 var app = express.createServer(express.logger());
-var content = buff.toString()
+var container = buff.toString()
 
 app.get('/', function(request, response) {  
-response.send(content);});
+response.send(container);});
 
 
 var port = process.env.PORT || 5000;
